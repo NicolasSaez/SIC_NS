29de7b04ee363254dbea007699d1c8e026163e6a
Nicolas Rolando Saez Matamala <nrsaez@sacyr.com>
2022-06-13 11:45:48 -0400

    13-06-2022

---
+++ Data/SitiosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class SitiosController
+    {
+        //esta es la entidad
+        SIC_NS_LOCALEntities context = new SIC_NS_LOCALEntities();
+
+        //GET
+        public List<Sitio> GetAllSitosActive(bool p_Activo = true)
+        {
+            return context.Sitio.Where(x => x.Activo == true).ToList();
+        }
+
+        //SET
+    }
+}
---
+++ Data/TipoSitiosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class TipoSitiosController
+    {
+        //esta es la entidad
+        SIC_NS_LOCALEntities context = new SIC_NS_LOCALEntities();
+
+        //GET
+        public List<TipoSitio> GetAllTipoSitosActive(bool p_Activo = true)
+        {
+            return context.TipoSitio.Where(x => x.Activo == true).ToList();
+        }
+
+
+        //SET
+    }
+}
+
